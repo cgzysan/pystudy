60faebce8a0fb78281c4f74676f13a6f2580063f
cgzysan <ysan1991@163.com>
2018-01-12 07:18:54 +0800

    1.12

---
+++ day07/class_method.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# by ysan
+
+'''
+    类方法，只能访问类变量，不能访问实例变量
+    @classmethod
+'''
---
+++ day07/property_method.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# by ysan
+
+'''
+    属性方法，把一个方法变成静态属性
+    @property
+'''
