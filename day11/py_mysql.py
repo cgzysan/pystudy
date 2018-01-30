486a3aa196c02b652c13bafe7e8ca32324651bd6
CaiGuozhu <ysan1991@163.com>
2018-01-30 21:02:31 +0800

    1.30

--- day11/py_mysql.py
+++ day11/py_mysql.py
@@ -19,6 +19,9 @@
 
     PyMySQL 是在 Python3.x 版本中用于连接 MySQL 服务器的一个库，Python2中则使用mysqldb。
     PyMySQL 遵循 Python 数据库 API v2.0 规范，并包含了 pure-Python MySQL 客户端库。
+
+    mysql 外键关联
+    外键约束：mysql通过外键约束来保证表与表之间的数据的完整性和准确性
 '''
 
 import pymysql
--- day13/jumpServer/models/models.py
+++ day13/jumpServer/models/models.py
@@ -3,3 +3,23 @@
   * @time: Created on 28/01/2018 1:45 PM
   * @author: by Ysan
 '''
+
+from sqlalchemy import Table, Column, Enum, Integer, String, DATE, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import ChoiceType
+
+Base = declarative_base()
+
+class Host(Base):
+    __tablename__ = 'host'
+    id = Column()
+
+
+class HostGroup(Base):
+    __tablename__ = 'host_group'
+
+
+class RemoteUser(Base):
+    __tablename__ = 'remote_user'
+
